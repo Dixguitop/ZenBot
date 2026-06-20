33cf87459b8e1e0ff6d44ffd7f7a4f9ec2dbc6a3
AxelDev09 <fxservidores@gmail.com>
2026-06-20 15:12:07 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+  botName: 'ZΞN-BOT',
+  ownerName: 'AXELDEV09',
+  version: '8.0.0',
+  prefix: /^[.#/!]/,
+  ownerNumber: ['5493772455367'],
+  MODE: 'public',
+  usePairingCode: true,
+  antiSpam: {
+    enabled: true,
+    maxCmds: 5,
+    ventanaMs: 8000,
+    muteMs: 15000,
+  },
+  antiSpamSubBot: {
+    enabled: true,
+    maxCmds: 5,
+    ventanaMs: 10000,
+    muteMs: 20000,
+  },
+  newsletterJid: '120363403631501323@newsletter',
+  groupLink: 'https://whatsapp.com/channel/0029Vb6OR9O2v1IvoXO5oT2c',
+  CURRENCY_NAME: 'ZenCoins',
+  CURRENCY_SYMBOL: '⌬',
+  PREMIUM_NAME: 'Kōgen',
+  PREMIUM_SYMBOL: '✦',
+  kogenPrice: 1000,
+  packname: 'ZΞN-BOT',
+  author: 'AXELDEV09',
+  limiteSubbots: '6',
+  footer: 'ᴢᴇɴ‑ʙᴏᴛ · ᴀxᴇʟᴅᴇᴠ⁰⁹'
+}
+
+export default config
+
+// NODE_ENV=production
